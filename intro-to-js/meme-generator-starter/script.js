1b1be970e764c48fb3b918dce285977014a6c7a7
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-28 20:24:52 +1000

    stack update

---
+++ intro-to-js/meme-generator-starter/script.js
@@ -0,0 +1,18 @@
+function getImage() {
+    let imageURL = document.getElementById("image-url").value;
+    console.log(imageURL);
+
+    let imageBox = document.createElement('img');
+    imageBox.id = "meme-image";
+    imageBox.src = imageURL;
+    let wrapper = document.getElementById('meme-image-wrapper');
+    wrapper.appendChild(imageBox);
+
+    document.getElementById("meme-builder").style.display = "block";
+}
+
+function addTopText() {
+    let topText = document.getElementById("top-text-input").value;
+    document.getElementById("top-text").innerText = topText;
+}
+    
--- intro-to-js/simple-message-starter/script.js
+++ intro-to-js/simple-message-starter/script.js
@@ -2,7 +2,9 @@ function showMessage() {
     let message = document.getElementById("message-text").value;
     let name = document.getElementById("message-name").value;
     let messageBox = document.getElementById("message-box");
-    messageBox.innerText = message + " - " + name;
+    // messageBox.innerText = message + " - " + name;
+    // formatted string below
+    messageBox.innerText = `${message} ${name}` 
 }
 
 // when the window loads, it's going to do something
