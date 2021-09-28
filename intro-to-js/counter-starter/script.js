749211e09588a6aa991429f26a77def374f68816
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-28 19:24:52 +1000

    starters

---
+++ intro-to-js/counter-starter/script.js
@@ -0,0 +1,57 @@
+// function add(step) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     // console.log("Found: ", step, number)
+//     number = parseInt(number) + step;
+//     // number = number + 1;
+//     numberElement.innerText = number;
+//     // console.log(number);
+// }
+
+// function subtract(step) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     // console.log("Found: ", step, number)
+//     number = parseInt(number) - step;
+//     // number = number + 1;
+//     numberElement.innerText = number;
+//     // console.log(number);
+// }
+
+// function multiply(step) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     // console.log("Found: ", step, number)
+//     number = parseInt(number) * step;
+//     // number = number + 1;
+//     numberElement.innerText = number;
+//     // console.log(number);
+// }
+
+// function divide(step) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     // console.log("Found: ", step, number)
+//     number = parseInt(number) / step;
+//     // number = number + 1;
+//     numberElement.innerText = number;
+//     // console.log(number);
+// }
+
+function math(step, method) {
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    if(method == 'subtract') {
+    number = parseInt(number) - step;
+    }
+    if(method == 'add') {
+    number = parseInt(number) + step;
+    }
+    if(method == 'multiply') {
+    number = parseInt(number) * step;
+    }
+    if(method == 'divide') {
+    number = parseInt(number) / step;
+    }
+    numberElement.innerText = number;
+}
---
+++ intro-to-js/simple-message-starter/script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    let message = document.getElementById("message-text").ariaValueMax;
+    console.log (message);
+}
+
+
+// Window onload is a value - 
+window.onload = function() {
+    console.log("page has loaded");
+}
