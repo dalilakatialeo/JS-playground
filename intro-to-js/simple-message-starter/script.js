460ae71134d62c431639d2fb9dbe69a6996bc77d
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-28 19:51:50 +1000

    js message challenge

--- intro-to-js/simple-message-starter/script.js
+++ intro-to-js/simple-message-starter/script.js
@@ -1,10 +1,19 @@
 function showMessage() {
-    let message = document.getElementById("message-text").ariaValueMax;
-    console.log (message);
+    let message = document.getElementById("message-text").value;
+    let name = document.getElementById("message-name").value;
+    let messageBox = document.getElementById("message-box");
+    messageBox.innerText = message + " - " + name;
 }
 
-
-// Window onload is a value - 
+// when the window loads, it's going to do something
 window.onload = function() {
-    console.log("page has loaded");
+    let speechBubbleElement = document.createElement("div");
+    speechBubbleElement.classList.add("speech-bubble");
+    let messageBox = document.createElement("p");
+    messageBox.innerText = "...";
+    messageBox.id = "message-box";
+    speechBubbleElement.appendChild(messageBox);
+
+    let outputElement = document.getElementById("output");
+    outputElement.appendChild(speechBubbleElement);
 }
