f8fcc41487526a21bf8b328caf3ba36ada29ab8c
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-02 10:56:35 +1000

    Update script.js

--- intro-to-js/playground-starter/script.js
+++ intro-to-js/playground-starter/script.js
@@ -30,3 +30,22 @@ function showName() {
 }
 
 showName(0)
+
+for (let count = 0; count < 10 ; count++) {
+    console.log(count);
+}
+
+let pets = ["Chilli", "Rex", "Roary", "Fleur"];
+console.log(pets);
+pets.push("Ivy");
+console.log(pets);
+console.log(pets[0]);
+console.log(pets[3]);
+
+for (let index = 0; index < pets.length; index++) {
+    console.log(pets[index]);
+}
+
+pets.forEach(pet => {
+    console.log(pet);
+});
