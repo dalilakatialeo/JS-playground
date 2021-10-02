5befb4fbc19b322c3b826252c003fc2cb486c9b8
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-02 10:37:06 +1000

    meme generator exercise

--- intro-to-js/meme-generator-starter/script.js
+++ intro-to-js/meme-generator-starter/script.js
@@ -1,12 +1,14 @@
 function getImage() {
     let imageURL = document.getElementById("image-url").value;
-    console.log(imageURL);
+    console.log(imageURL)
+// }
 
     let imageBox = document.createElement('img');
     imageBox.id = "meme-image";
     imageBox.src = imageURL;
     let wrapper = document.getElementById('meme-image-wrapper');
     wrapper.appendChild(imageBox);
+// }
 
     document.getElementById("meme-builder").style.display = "block";
 }
@@ -15,4 +17,8 @@ function addTopText() {
     let topText = document.getElementById("top-text-input").value;
     document.getElementById("top-text").innerText = topText;
 }
-    
+
+function addBottomText() {
+    let bottomText = document.getElementById("bottom-text-input").value;
+    document.getElementById("bottom-text").innerText = bottomText;
+}
