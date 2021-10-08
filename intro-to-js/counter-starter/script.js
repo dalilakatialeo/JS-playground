96bea180b3bae7a19a8e46a7ffbce7c975f50d94
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-08 23:27:02 +1000

    counter

--- intro-to-js/counter-starter/script.js
+++ intro-to-js/counter-starter/script.js
@@ -1,19 +1,19 @@
-// function add(step) {
-//     let numberElement = document.getElementById("number");
-//     let number = numberElement.innerText;
-//     // console.log("Found: ", step, number)
-//     number = parseInt(number) + step;
-//     // number = number + 1;
-//     numberElement.innerText = number;
-//     // console.log(number);
-// }
+function add(step) {
+    let numberElement = document.getElementById("number");
+    let number = numberElement.innerText;
+    console.log(number)
+    number = parseInt(number) + step;
+    // number = number + 1;
+    numberElement.innerText = number;
+    console.log(number);
+}
 
 // function subtract(step) {
 //     let numberElement = document.getElementById("number");
 //     let number = numberElement.innerText;
 //     // console.log("Found: ", step, number)
 //     number = parseInt(number) - step;
-//     // number = number + 1;
+//     // number = number - 1;
 //     numberElement.innerText = number;
 //     // console.log(number);
 // }
@@ -38,20 +38,20 @@
 //     // console.log(number);
 // }
 
-function math(step, method) {
-    let numberElement = document.getElementById("number");
-    let number = numberElement.innerText;
-    if(method == 'subtract') {
-    number = parseInt(number) - step;
-    }
-    if(method == 'add') {
-    number = parseInt(number) + step;
-    }
-    if(method == 'multiply') {
-    number = parseInt(number) * step;
-    }
-    if(method == 'divide') {
-    number = parseInt(number) / step;
-    }
-    numberElement.innerText = number;
-}
+// function math(step, method) {
+//     let numberElement = document.getElementById("number");
+//     let number = numberElement.innerText;
+//     if(method == 'subtract') {
+//     number = parseInt(number) - step;
+//     }
+//     if(method == 'add') {
+//     number = parseInt(number) + step;
+//     }
+//     if(method == 'multiply') {
+//     number = parseInt(number) * step;
+//     }
+//     if(method == 'divide') {
+//     number = parseInt(number) / step;
+//     }
+//     numberElement.innerText = number;
+// }
